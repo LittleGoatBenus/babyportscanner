eaaca108fb6be1e374fee7ee34e490882b4c6760
Luka <68807802+LittleGoatBenus@users.noreply.github.com>
2020-08-03 20:27:59 +0200

    Update portscanner.py

--- portscanner.py
+++ portscanner.py
@@ -9,7 +9,7 @@ if len(sys.argv) == 2:
     target = socket.gethostbyname(sys.argv[1]) #translates hostname to IPV4
 else:
     print("invalid")
-    print("Syntax : python3 scanner.py <IP>, no ip given")
+    print("Syntax : python3 scanner.py <IP>, no ip given") # if a wrong ip is given it will print this error
     sys.exit()
 
 #add a pretty banner
@@ -29,17 +29,17 @@ very slow, v1.0
 """)
 
 print("scanning..."+target)
-print("time initiated", str(datetime.now()))
+print("time initiated", str(datetime.now()))#prints the initiated time the program was run
 print('_' * 50)
 
 try:
     for port in range(50,85): #port range 50 - 85
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        socket.setdefaulttimeout(1) # if port is closed it will move on
+        socket.setdefaulttimeout(1) # if port is closed it will move on after 1 second
         result = s.connect_ex((target, port)) # returns an error indicator
-        print()
-        if result == 0: # if a port is open a 0 will return this shows that 0 returns  and it will print the open port
-            print("port {} is open".format(port))
+        print("analysing port{}".format(port)) #prints the port it is checking if open
+        if result == 0: # if a port is open the program will return a 0 assigned to that port, the if statement shows that is a 0 is returned it will print the open port
+            print("port {} is open".format(port)) #prints a return value if the port is open 
         s.close()
 except KeyboardInterrupt:
     print("\nExit due to interupt")
